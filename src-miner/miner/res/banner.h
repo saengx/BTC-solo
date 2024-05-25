4d055f412367e1661e83296e973c44579dfdadbe
saengx <157899625+saengx@users.noreply.github.com>
2024-05-26 03:29:15 +0700

    Update banner.h

--- src-miner/miner/res/banner.h
+++ src-miner/miner/res/banner.h
@@ -3,9 +3,9 @@ static void show_credits()
 {
 printf(CL_LCY" **"PACKAGE_NAME""CL_LYL" "PACKAGE_VERSION CL_LCY" by PICHET SAENGTAWAN**\n");
 printf(CL_N"------------------------------------------------\n\n");
-printf(CL_LCY"  Developer  "CL_LYL"          : PICHET SAENGTAWAN\n");
-printf(CL_LCY"  Facebook"CL_LYL"             : https://www.facebook.com/pichet.saengtawan\n");
-printf(CL_LCY"  Youtube Channel"CL_LYL"      : Android Phone Mining\n");
-printf(CL_LCY"  สนับสนุนกัน เพื่อรับ update"CL_LYL": กสิกรไทย 8432075665 พิเชษฐ์ แสงตะวัน\n");
+printf(CL_LCY"  Developer  "CL_LYL"    : PICHET SAENGTAWAN\n");
+printf(CL_LCY"  Facebook"CL_LYL"       : Pichet Saengtawan\n");
+printf(CL_LCY"  Youtube Channel"CL_LYL": Android Phone Mining\n");
+printf(CL_LCY"  สนับสนุน นักพัฒนา"CL_LYL": กสิกรไทย 8432075665 พิเชษฐ์ แสงตะวัน\n");
 printf(CL_N"------------------------------------------------\n");	
 }
