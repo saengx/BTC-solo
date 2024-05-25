f92dc9d56430f0edf97d8268563e3eaf65a3dfaa
saengx <157899625+saengx@users.noreply.github.com>
2024-05-26 03:44:48 +0700

    Update banner.h

--- src-miner/miner/res/banner.h
+++ src-miner/miner/res/banner.h
@@ -6,6 +6,6 @@ printf(CL_N"------------------------------------------------\n\n");
 printf(CL_LCY"  Developer  "CL_LYL"    : PICHET SAENGTAWAN\n");
 printf(CL_LCY"  Facebook"CL_LYL"       : Pichet Saengtawan\n");
 printf(CL_LCY"  Youtube Channel"CL_LYL": Android Phone Mining\n");
-printf(CL_LCY"  สนับสนุน นักพัฒนา"CL_LYL": กสิกรไทย 8432075665 พิเชษฐ์ แสงตะวัน\n");
+printf(CL_LCY"  สนับสนุนนักพัฒนา"CL_LYL": กสิกรไทย 8432075665 พิเชษฐ์ แสงตะวัน\n");
 printf(CL_N"------------------------------------------------\n");	
 }
