087aadcb3363e24fb26cb3b3ddbe029986735757
saengx <157899625+saengx@users.noreply.github.com>
2024-05-25 23:55:35 +0700

    Update main.py

--- src-miner/main.py
+++ src-miner/main.py
@@ -3,17 +3,8 @@ import json
 import time
 import pip
 from config import banner
-try:
-    from progress.bar import ShadyBar
-except ImportError:
-    pip.main(['install', '--user', 'progress'])
-    from progress.bar import ShadyBar
+from progress.bar import ShadyBar
 
-try:
-    import requests
-except ImportError:
-    pip.main(['install', '--user', 'requests'])
-    import requests  
 def runOffline():
     banner()
     try:
